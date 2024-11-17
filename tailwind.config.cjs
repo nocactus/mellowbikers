da3950c0e9a756b2bbfc7c67a64d9cc8067170a8
Timo <timo@nocactus.nl>
2024-11-17 20:54:33 +0100

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'], // Scan deze bestanden op Tailwind-klassen
+  theme: {
+    extend: {}, // Gebruik dit om thema-aanpassingen te maken
+  },
+  plugins: [], // Voeg Tailwind-plugins toe als nodig
+};
+
